0ce28d2370405120509ac5def685b306e4f9ab1d
Igor Becker <igor_nyu@hotmail.com>
2022-04-28 14:59:48 -0300

    adding some objects

--- src/js/Overworld.js
+++ src/js/Overworld.js
@@ -10,6 +10,23 @@ class Overworld {
     image.onload = () => {
       this.ctx.drawImage(image, 0, 0)
     }
-    image.src = '/assets/maps/DemoLower.png'
+    image.src = 'src/assets/maps/DemoLower.png'
+
+    const heroVector = {
+      x: 5,
+      y: 6
+    }
+
+    const heroShadow = new Image()
+    heroShadow.onload = () => {
+      this.ctx.drawImage(heroShadow, heroVector.x * 16 - 8, heroVector.y * 16 - 18)
+    }
+    heroShadow.src = 'src/assets/characters/shadow.png'
+
+    const hero = new Image()
+    hero.onload = () => {
+      this.ctx.drawImage(hero, 0, 0, 32, 32, heroVector.x * 16 - 8, heroVector.y * 16 - 18, 32, 32)
+    }
+    hero.src = 'src/assets/characters/people/hero.png'
   }
 }
